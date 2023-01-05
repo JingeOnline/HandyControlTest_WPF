54cdfd1b49a53a68a8516580d4debb9c8703e02a
Jinge <jinge_windows@hotmail.com>
2023-01-05 16:07:53 +1100

    Add project files.

---
+++ HandyControlTest_WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using HandyControl.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandyControlTest_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Growl_Click(object sender, RoutedEventArgs e)
+        {
+            Growl.Success("File saved successfully!");
+        }
+
+        private void Button_GrowlGlobal_Click(object sender, RoutedEventArgs e)
+        {
+            Growl.InfoGlobal("GrowlInfo");
+        }
+    }
+}
